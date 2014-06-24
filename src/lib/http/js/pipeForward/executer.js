047656e0df3e2425f1264bc84c21c8cb264b8967
Michael VERGOZ <mv@verman.fr>
2014-06-24 14:05:09 +0400

    Add executer support #13

---
+++ src/lib/http/js/pipeForward/executer.js
@@ -0,0 +1,10 @@
+var executer = function(gjs) { }
+
+executer.request = function(gjs, callback) {
+	callback(gjs);
+}
+
+executer.ctor = function(gjs) {
+}
+
+module.exports = executer; 
---
+++ src/lib/http/js/pipeReverse/executer.js
@@ -0,0 +1,1 @@
+../pipeForward/executer.js
