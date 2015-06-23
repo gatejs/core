fb4575bbbcae89c0280e243a754971f22adde732
mykiimike <mv@verman.fr>
2015-06-23 11:10:22 +0200

    Add HSTS opcode for reverse site

---
+++ src/lib/http/js/pipeReverse/hsts.js
@@ -0,0 +1,32 @@
+var hsts = function(gjs) { }
+
+hsts.request = function(pipe, options) {
+	if(pipe.server.config.ssl != true)
+		return;
+	
+	if(!options)
+		options = {};
+	
+	var age, subdom, preload;
+	
+	age = 10512000;
+	if(options.age > 0)
+		age = options.age;
+	
+	subdom = " includeSubDomains;";
+	if(options.subDomain == false)
+		subdom = "";
+	
+	preload = " preload;";
+	if(options.preload == false)
+		preload = "";
+	
+	pipe.response.on('response', function(res, from) {
+		res.gjsSetHeader("Strict-Transport-Security", "max-age="+age+subdom+preload);
+	});
+
+}
+
+hsts.ctor = function(gjs) { }
+
+module.exports = hsts; 
