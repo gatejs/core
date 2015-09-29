1be4c57aed18990ec9152699a9da0ec65e4dd8a2
Maxime Ferrino <mf@verman.fr>
2015-09-29 10:47:28 +0400

    Catched an error that node may emit.

--- src/lib/http/js/pipeReverse/proxyPass.js
+++ src/lib/http/js/pipeReverse/proxyPass.js
@@ -317,6 +317,13 @@ proxyPass.request = function(pipe, proxyname) {
 			if(!nodePtr.retry)
 				nodePtr.retry = 3;
 			
+			socket.on('error', function(e) {
+				pipe.root.lib.gateWaf.siteInfo(pipe.root,
+					pipe.site.name, 
+					'Server socket error (from '+pipe.request.connection.remoteAddress+') : '+e
+				);
+			});
+			
 			socket.timeoutId = setTimeout(
 				socketErrorDetection, 
 				nodePtr.timeout*1000, 
