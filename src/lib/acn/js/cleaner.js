196b5b74aa567d11d507d632d890a4f5e31b2897
mykiimike <mv@verman.fr>
2017-02-28 09:32:47 +0100

    Disabling ACN cleaner

--- src/lib/acn/js/cleaner.js
+++ src/lib/acn/js/cleaner.js
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) 2010-2014 BinarySEC SAS
  * Associative Cache Network cleaner [http://www.binarysec.com]
- * 
+ *
  * This file is part of Gate.js.
- * 
+ *
  * Gate.js is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -26,20 +26,22 @@ var cleaner = function(gjs) {}
 
 cleaner.loader = function(gjs) {
 	cleaner.gjs = gjs;
+
+	return;
 	
 	var delay = 43200000;
 	var interval = 250;
-	
+
 	if(gjs.serverConfig.acn) {
 		if(gjs.serverConfig.acn.cleanDelay)
 			delay = gjs.serverConfig.acn.cleanDelay*1000;
 		if(gjs.serverConfig.acn.cleanInterval)
 			interval = gjs.serverConfig.acn.cleanInterval;
 	}
-	
+
 	if(cluster.isMaster) {
 		var reference = 0;
-		
+
 		function processFile(args) {
 			var hdr = gjs.lib.acn.loadHeaderFile(args.file),
 			isF = gjs.lib.acn.isFresh(hdr);
@@ -50,10 +52,10 @@ cleaner.loader = function(gjs) {
 			}
 			reference--;
 		}
-		
+
 		function processDir(dir) {
 			try {
-				
+
 				var d = fs.readdirSync(dir), a;
 				var inInterval = interval;
 				for(a in d) {
@@ -74,7 +76,7 @@ cleaner.loader = function(gjs) {
 			} catch(e) { /* do nothing */ }
 			reference--;
 		}
-    
+
 		function recycle() {
 			reference = 0;
 			setInterval(function() {
@@ -85,14 +87,12 @@ cleaner.loader = function(gjs) {
 			}, delay);
 			processDir(gjs.lib.acn.cacheDir);
 		}
-		
+
 		setTimeout(recycle, delay);
 	}
-	
-	
+
+
 }
 
 
 module.exports = cleaner;
-
-
