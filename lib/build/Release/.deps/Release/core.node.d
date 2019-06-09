8ab0e158eb3b8be3ddfbb3316efebf300498e5f6
Michael Vergoz <m.vergoz@vergoz.ch>
2019-06-09 14:43:02 +0200

    Deleting building files

--- lib/build/Release/.deps/Release/core.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/core.node := rm -rf "Release/core.node" && cp -af "Release/obj.target/core.node" "Release/core.node"
--- lib/build/Release/.deps/Release/obj.target/core.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/core.node := g++ -shared -pthread -rdynamic -m64  -Wl,-soname=core.node -o Release/obj.target/core.node -Wl,--start-group Release/obj.target/core/nreg/utils.o Release/obj.target/core/nreg/module.o Release/obj.target/core/nreg/nreg.o Release/obj.target/core/nreg/nreg_wrap.o Release/obj.target/core/nreg/unicode.o -Wl,--end-group 
